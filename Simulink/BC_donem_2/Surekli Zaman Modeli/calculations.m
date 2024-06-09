567844d95d03c06bd77cf2e3b1fd6b45e0e93b7c
AltayAtaman <redtayal@gmail.com>
2024-06-09 05:47:51 +0300

    Pole&Zero Model, Simulink&Matlab Benzetim
    Hem matlab hem de simulink uzerinden uzun olan ve pole ve zero'ya sahip transfer fonksiyonu modellendi ve ikisinin de ciktilarinin yakin oldugu gozlemlendi.

--- Simulink/BC_donem_2/Surekli Zaman Modeli/calculations.m
+++ Simulink/BC_donem_2/Surekli Zaman Modeli/calculations.m
@@ -1,17 +1,37 @@
-%% Final Value Theorem:
-total_system = tf(kt, [L*Y1, L*Y2 + R*Y1, L*Y3 + R*Y2 + kt*ke, R*Y3]);
-system_input = tf(1, [1, 0]); 
+%% Transfer Fonksiyonu Eldesi
 
-open_loop_response = total_system * system_input;
+tf_dc_num = [kt * J, kt * b];
+tf_dc_denom = [L * J, (L * b) + (R * J), (R * b) + (ke * kt)];
 
-% Apply final value theorem
+tf_mech_num = 1;
+tf_mech_denom = [Y1, Y2, Y3];
+
+% DC Motor ve Mekanik Alt Sistem TF.
+tf_dc = tf(tf_dc_num, tf_dc_denom);
+tf_mech = tf(tf_mech_num, tf_mech_denom);
+
+% Tum sistem TF.
+total_system = tf_dc * tf_mech;
+
+%% Kutuplarin Bulunmasi
+poles = pole(total_system);
+fprintf("Poles: "); poles_str = sprintf('%.4e ', poles);
+disp(poles_str);
+
+%% Basamak Cevabi
+figure;
+step(10 * total_system);
+title('Step Response o');
+grid on;
+
+%% Son Deger Teoremi
 syms s;
 [num, den] = tfdata(open_loop_response, 'v');
 G_s = poly2sym(num, s) / poly2sym(den, s); 
 final_value = limit(s * G_s, s, 0); 
 final_value_numeric = double(final_value);
 
-% Compute and display the settling time
+%% Yerlesme Zamani Hesabi
 step_info = stepinfo(total_system);
 settling_time = step_info.SettlingTime;
 
