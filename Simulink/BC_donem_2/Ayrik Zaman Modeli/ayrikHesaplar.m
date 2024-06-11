68d25b9bcc3fb065fa70aa7068d8682c51ba6131
AltayAtaman <redtayal@gmail.com>
2024-06-11 20:54:57 +0300

    ayrik denklemi degismedi, gerilim 80-100 arasi cikiyor

--- Simulink/BC_donem_2/Ayrik Zaman Modeli/ayrikHesaplar.m
+++ Simulink/BC_donem_2/Ayrik Zaman Modeli/ayrikHesaplar.m
@@ -1,7 +1,6 @@
 %% Ayrik Kontrolcu Girdileri:
 
-dt = 0.1;
-Ae = 0.5;
+dt = 0.01;
 x_0 = 0; x_0_dot = 0;
 
 fx_now = (2 .* y1 / (dt .^ 2)) - y3;
@@ -11,14 +10,17 @@ fx_fut = (y1 / (dt .^ 2)) + (y2 / (2 .* dt));
 h_fut_2 = z1 / dt^3;
 h_fut = (z2 / dt^2) + (z3 / (2 * dt)) - ((3 * z1) / dt^3);
 h_now = ((3 * z1) / dt^3) - ((2 * z2) / dt^2);
-h_prev = (z2 / dt^2) - (z3 / (2 * dt)) - (z1 / dt^3);
+h_pre = (z2 / dt^2) - (z3 / (2 * dt)) - (z1 / dt^3);
 
 constants = [fx_now
              fx_pre
              fx_fut
+             h_fut_2
+             h_fut
+             h_now
+             h_pre
              x_0
              x_0_dot
-             Ae
              N1
              N2
              N3];       
